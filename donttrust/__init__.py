6400822b42a31fce52ad86f82e1bb02391f3fb9c
arnu515 <arnu5152@gmail.com>
2021-01-07 12:02:16 +0530

    add support for dictionaries

--- donttrust/__init__.py
+++ donttrust/__init__.py
@@ -35,45 +35,53 @@ class DontTrust:
             self._items[key] = items[key]
             self._items[key].field = key
 
-    def validate(self, **kwargs):
+    def validate(self, dict_=None, **kwargs):
         """
         Validates all schema in this object and throws a ``donttrust.ValidationError`` if validation fails.
         To not raise an error and return ``False`` if validation fails, use ``validate_without_exception``.
 
+        :param dict_: The dictionary of items to validate. Use either this or kwargs
         :param kwargs: The items to validate. It should be a kwargs object with the same fields as the schema.
         :returns: Returns a dictionary with the validated items.
         :raises: Raises ``donttrust.ValidationError`` if validation fails
         """
         final = dict()
         try:
-            for key in self._items.keys():
-                final[key] = self._items[key].validate(kwargs.get(key))
+            if dict_ is None:
+                for key in self._items.keys():
+                    final[key] = self._items[key].validate(kwargs.get(key))
+            else:
+                for key in self._items.keys():
+                    final[key] = self._items[key].validate(dict_.get(key))
             return final
         except DontTrustBaseException as e:
             raise ValidationError(e.field, e.message)
 
-    def validate_and_return_json_object(self, **kwargs) -> dict:
+    def validate_and_return_json_object(self, dict_=None, **kwargs) -> dict:
         """
         Same as ``validate``, but returns a dictionary instead of throwing errors.
 
         Returns ``{"data": data}`` if validation succeedes, or ``{"error": message, "field": fieldname}`` if validation fails
 
+        :param dict_: The dictionary of items to validate. Use either this or kwargs
         :param kwargs: The items to validate. It should be a kwargs object with the same fields as the schema.
         :returns: The dictionary
         """
         try:
-            final = self.validate(**kwargs)
+            final = self.validate(dict_, **kwargs)
             return {"data": final}
         except ValidationError as e:
             return {"error": e.message, "field": e.field}
 
-    def validate_without_exception(self, **kwargs):
+    def validate_without_exception(self, dict_=None, **kwargs):
         """
         Same as ``validate``, but returns ``False`` if validation fails
+
+        :param dict_: The dictionary of items to validate. Use either this or kwargs
         :param kwargs: The items to validate. It should be a kwargs object with the same fields as the schema.
         :return: Returns a dictionary with validated data if validation succeeds, else returns False
         """
         try:
-            return self.validate(**kwargs)
+            return self.validate(dict_, **kwargs)
         except ValidationError:
             return False
--- tests/test_main.py
+++ tests/test_main.py
@@ -8,12 +8,19 @@ class Test(unittest.TestCase):
         trust = DontTrust(username=Schema().string().alphanum().lower().required(), verified=Schema().boolean(),
                           password=Schema().string().required(), email=Schema().email().required().allow_tlds("com"),
                           access_token=Schema().string().alphanum().lower(), role=Schema().string().default("member"))
-        self.assertTrue(trust.validate(username="asdsad", password="sasdfghj", email="a@b.com"))
+        self.assertTrue(trust.validate_without_exception(username="asdsad", password="sasdfghj", email="a@b.com"))
         self.assertFalse(trust.validate_without_exception(username="i"))
         self.assertTrue(trust.validate_without_exception(username="i", password="sadsdadsa", access_token="sadasdasd",
                                                           email="test@gmail.com", verified=True))
         self.assertFalse(trust.validate_without_exception(username="i", access_token="sadasdasd",
                                                           email="test@gmail.com", verified=True))
+
+    def test2(self):
+        trust = DontTrust(username=Schema().string().alphanum().lower().required().strip(),
+                          password=Schema().string().required())
+        self.assertTrue(trust.validate_without_exception({"username": "abcd", "password": "hello"}))
+        self.assertFalse(trust.validate_without_exception({"username": "abcd"}))
+        self.assertFalse(trust.validate_without_exception({"password": "abcd"}))
 
 
 if __name__ == '__main__':
