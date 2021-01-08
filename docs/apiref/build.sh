0096d07d6362980e0a3d843278687b218c5b2f43
arnu515 <arnu5152@gmail.com>
2021-01-08 09:21:57 +0530

    purge build directory if it exists

--- docs/apiref/build.sh
+++ docs/apiref/build.sh
@@ -1,6 +1,11 @@
+if [ -d build ]; then
+    echo "Build directory already exists. Purging..."
+    rm -r build
+fi
 cd ../..
 source venv/bin/activate
 sphinx-apidoc -f -o docs/apiref/source/docstrings donttrust
 cd docs/apiref
 make html
 mv build/html build/apiref
+mv build/apiref ../static
